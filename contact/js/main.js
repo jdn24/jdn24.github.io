3f5ea68a0aa6aede2f75869b27ae19b70ed1ab27
Jaden Ade <80100672+jdn24@users.noreply.github.com>
2024-05-13 17:02:34 +0400

    added folders for new directory

---
+++ contact/js/main.js
@@ -0,0 +1,35 @@
+/* ===================================================================
+ * Imminent 1.0.0 - Main JS
+ *
+ * ------------------------------------------------------------------- */
+
+(function($) {
+
+    "use strict";
+    
+    const cfg = {
+                scrollDuration : 800, // smoothscroll duration
+                mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc' // mailchimp url
+                };
+    const $WIN = $(window);
+
+    // Add the User Agent to the <html>
+    // will be used for IE10/IE11 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0; rv:11.0))
+    const doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+   /* Block right click 
+* ---------------------------------------------------- */
+    document.addEventListener('contextmenu', event => {
+        event.preventDefault();
+    });
+    
+    document.querySelectorAll('.disabled').forEach(element => {
+        element.style.pointerEvents = 'none';
+    });
+
+  
+   
+
+})(jQuery);
