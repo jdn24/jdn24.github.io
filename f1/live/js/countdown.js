3cb028d30682acd7d2916ba3323827a1b0da407b
Jaden Ade <jadenA@amitydubai.ae>
2024-07-26 21:06:36 +0400

    added dutch timings

--- f1/live/js/countdown.js
+++ f1/live/js/countdown.js
@@ -1,19 +1,20 @@
 window.onload = function() {
     // Define your online periods
     const onlinePeriods = [
-        //Hungary GP
-        { gpname: 'Hugarian', racename:'Practice 1 Session',start: '2024-07-19T15:00:00+04:00', end: '2024-07-19T17:00:00+04:00' }, //Practice 1
-        { gpname: 'Hugarian', racename:'Practice 2 Session',start: '2024-07-19T18:30:00+04:00', end: '2024-07-19T20:30:00+04:00' }, //Practice 2
-        { gpname: 'Hugarian', racename:'Practice 3 Session',start: '2024-07-20T14:00:00+04:00', end: '2024-07-20T16:00:00+04:00' }, //Practice 3
-        { gpname: 'Hugarian', racename:'Qualifying Session',start: '2024-07-20T17:30:00+04:00', end: '2024-07-20T19:30:00+04:00' }, //Quali
-        { gpname: 'Hugarian', racename:'Race',start: '2024-07-21T16:30:00+04:00', end: '2024-07-21T19:30:00+04:00' }, //Race
-
+        
         //Belgium GP
         { gpname: 'Belgian', racename:'Practice 1 Session',start: '2024-07-26T15:00:00+04:00', end: '2024-07-26T17:00:00+04:00' }, //Practice 1
         { gpname: 'Belgian', racename:'Practice 2 Session',start: '2024-07-26T18:30:00+04:00', end: '2024-07-26T20:30:00+04:00' }, //Practice 2
         { gpname: 'Belgian', racename:'Practice 3 Session',start: '2024-07-27T14:00:00+04:00', end: '2024-07-27T16:00:00+04:00' }, //Practice 3
         { gpname: 'Belgian', racename:'Qualifying Session',start: '2024-07-27T17:30:00+04:00', end: '2024-07-27T19:30:00+04:00' }, //Quali
         { gpname: 'Belgian', racename:'Race',start: '2024-07-28T16:30:00+04:00', end: '2024-07-28T19:30:00+04:00' }, //Race
+
+        //Dutch GP
+        { gpname: 'Dutch', racename:'Practice 1 Session',start: '2024-08-23T14:00:00+04:00', end: '2024-08-23T16:00:00+04:00' }, //Practice 1
+        { gpname: 'Dutch', racename:'Practice 2 Session',start: '2024-08-23T17:30:00+04:00', end: '2024-08-23T19:30:00+04:00' }, //Practice 2
+        { gpname: 'Dutch', racename:'Practice 3 Session',start: '2024-08-24T13:00:00+04:00', end: '2024-08-24T15:00:00+04:00' }, //Practice 3
+        { gpname: 'Dutch', racename:'Qualifying Session',start: '2024-08-24T16:30:00+04:00', end: '2024-08-24T18:30:00+04:00' }, //Quali
+        { gpname: 'Dutch', racename:'Race',start: '2024-08-25T16:30:00+04:00', end: '2024-08-25T19:30:00+04:00' }, //Race
     ];
     showoverlay();
     function showoverlay(){
